98d325a87fbd12ad9013f314e02a24cdc6952ab8
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2023-11-03 01:29:06 +0900

    [wip] regex

---
+++ register-regex/src/engine/parser.rs
@@ -0,0 +1,81 @@
+//! parse regular expression, convert into AST
+use std::{
+    error::Error,
+    fmt::{self, Display},
+    mem::take,
+};
+
+/// type representing AST
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>),
+}
+
+/// type representing Parsing Errors
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidEscape(usize, char),
+    InvalidRightParen(usize),
+    NoPrev(usize),
+    NoRightParen,
+    Empty,
+}
+
+/// implementation of Display trait for Displaying Parsing Error
+impl Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ParseError::InvalidEscape(pos, c) => {
+                write!(f, "ParseError: invalid escape: pos = {pos}, char = '{c}'",)
+            }
+            ParseError::InvalidRightParen(pos) => {
+                write!(f, "ParseError: invalid right parenthesis: pos = {pos}")
+            }
+            ParseError::NoPrev(pos) => write!(f, "ParseError: no previous expression: pos = {pos}"),
+            ParseError::NoRightParen => write!(f, "ParseError: no right parenthesis"),
+            ParseError::Empty => write!(f, "ParseError: empty expression"),
+        }
+    }
+}
+
+impl Error for ParseError {}
+
+/// escape special char
+fn parse_escape(pos: usize, c: char) -> Result<AST, ParseError> {
+    match c {
+        '\\' | '(' | ')' | '|' | '+' | '*' | '?' => Ok(AST::Char(c)),
+        _ => {
+            let err = ParseError::InvalidEscape(pos, c);
+            Err(err)
+        }
+    }
+}
+
+enum PSQ {
+    Plus,
+    Star,
+    Question,
+}
+
+/// error when pattern does not exist before PSQ
+fn parse_plus_star_question(
+    seq: &mut Vec<AST>,
+    ast_type: PSQ,
+    pos: usize,
+) -> Result<(), ParseError> {
+    if let Some(prev) = seq.pop() {
+        let ast = match ast_type {
+            PSQ::Plus => AST::Plus(Box::new(prev)),
+            PSQ::Star => AST::Star(Box::new(prev)),
+            PSQ::Question => AST::Question(Box::new(prev)),
+        };
+        seq.push(ast);
+        Ok(())
+    } else {
+        Err(ParseError::NoPrev(pos))
+    }
+}
